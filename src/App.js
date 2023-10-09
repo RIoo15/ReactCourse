a80e213413805bde6b9e7cd0923795cc897c67d6
RIoo15 <hashim74100@gmail.com>
2023-10-09 11:44:02 +0500

    Improving Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light') //whether dark mode is enabled or not
+  const toogleMode = () =>{
+    if (mode === 'light'){
+      setMode ('dark')
+      document.body.style.backgroundColor = '#042743'
+    }
+    else {
+      setMode  ('light')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
+
   return (
     <>
-<Navbar title="TextUtils" aboutText="About TextUtils" />
+<Navbar title="TextUtils" mode={mode} toogleMode={toogleMode} />
 <div className="container my-3">
-  <TextForm heading="Enter the text to analyze below" />
+  <TextForm heading="Enter the text to analyze below"  mode={mode} />
 </div>
 
 {/* <About/> */}
@@ -16,7 +29,6 @@ function App() {
     </>
   );
 }
-//HASHIM
-//HAMZA 
+
 export default App;
 
